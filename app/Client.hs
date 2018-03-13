43d1635bf7f74d4f123907d4ab2f5ab2a9319cb3
Ceri Storey <ceri.storey@oliverwyman.com>
2018-03-13 10:43:15 +0000

    Lift decls to top level.

--- app/Client.hs
+++ app/Client.hs
@@ -8,37 +8,46 @@ import qualified Control.Exception as E
 import Network.Socket hiding (recv)
 import qualified System.IO.Streams         as Streams
 import qualified System.IO.Streams.Binary  as BStreams
+import qualified Data.Binary as Binary
 
 main :: IO ()
 main = withSocketsDo $ do
     addr <- resolve "127.0.0.1" "3000"
     E.bracket (open addr) close talk
-  where
-    resolve host port = do
-        let hints = defaultHints { addrSocketType = Stream }
-        addr:_ <- getAddrInfo (Just hints) (Just host) (Just port)
-        return addr
-    open addr = do
-        sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-        connect sock $ addrAddress addr
-        return sock
 
-    streamsOf client = do
-        (is, os) <- (Streams.socketToStreams client)
-        eis <- BStreams.decodeInputStream is
-        eos <- BStreams.encodeOutputStream os
+resolve :: HostName -> ServiceName -> IO AddrInfo
+resolve host port = do
+    let hints = defaultHints { addrSocketType = Stream }
+    addr:_ <- getAddrInfo (Just hints) (Just host) (Just port)
+    return addr
 
-        return (eis, eos)
-    talk sock = do
-        (is, os) <- streamsOf sock
-        Streams.write (Just Lib.Bing) os
-        go is os
-    go is os = do
-        msg <- (Streams.read is :: IO (Maybe Lib.Message))
-        putStr "Received: "
-        putStrLn $ show msg
-        case msg of
-            Just x -> do
-                Streams.write (Just x) os
-                go is os
-            Nothing -> return ()
+open :: AddrInfo -> IO Socket
+open addr = do
+    sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+    connect sock $ addrAddress addr
+    return sock
+
+streamsOf :: (Binary.Binary a, Binary.Binary b) => Socket -> IO (Streams.InputStream a, Streams.OutputStream b)
+streamsOf client = do 
+    (is, os) <- (Streams.socketToStreams client)
+    eis <- BStreams.decodeInputStream is
+    eos <- BStreams.encodeOutputStream os
+
+    return (eis, eos)
+
+talk :: Socket -> IO ()
+talk sock = do
+    (is, os) <- streamsOf sock
+    Streams.write (Just Lib.Bing) os
+    go is os
+
+go :: Streams.InputStream Lib.Message -> Streams.OutputStream Lib.Message -> IO ()
+go is os = do
+    msg <- (Streams.read is :: IO (Maybe Lib.Message))
+    putStr "Received: "
+    putStrLn $ show msg
+    case msg of
+        Just x -> do
+            Streams.write (Just x) os
+            go is os
+        Nothing -> return ()
--- app/Server.hs
+++ app/Server.hs
@@ -5,6 +5,7 @@ import qualified Lib
 import qualified Network.Socket            as S
 import qualified System.IO.Streams         as Streams
 import qualified System.IO.Streams.Binary  as BStreams
+import qualified Data.Binary as Binary
 
 -- import qualified Data.ByteString as B
 
@@ -17,48 +18,52 @@ main = S.withSocketsDo $ do
     addr <- resolve "3000"
     E.bracket (listenFor addr) S.close accepter
     return ()
-  where
-    resolve port = do
-        let hints = S.defaultHints {
-              S.addrFlags = [S.AI_PASSIVE]
-            , S.addrSocketType = S.Stream
-            }
-        addr:_ <- S.getAddrInfo (Just hints) Nothing (Just port)
-        return addr
 
-    listenFor addr = do
-        sock <- S.socket (S.addrFamily addr) (S.addrSocketType addr) (S.addrProtocol addr)
-        S.setSocketOption sock S.ReuseAddr 1
-        S.bind sock (S.addrAddress addr)
-        S.listen sock 10
-        putStrLn . show =<< S.getSocketName sock
-        return sock
+resolve :: S.ServiceName -> IO S.AddrInfo
+resolve port = do
+    let hints = S.defaultHints {
+            S.addrFlags = [S.AI_PASSIVE]
+        , S.addrSocketType = S.Stream
+        }
+    addr:_ <- S.getAddrInfo (Just hints) Nothing (Just port)
+    return addr
 
-    accepter listener = do
-        void $ forever $ do
-            (client, x) <- S.accept listener
-            putStrLn $ show (client, x)
-            C.forkIO $ do
-                E.bracket (streamsOf client) (const $ S.close client) handleClient
+listenFor :: S.AddrInfo -> IO S.Socket
+listenFor addr = do
+    sock <- S.socket (S.addrFamily addr) (S.addrSocketType addr) (S.addrProtocol addr)
+    S.setSocketOption sock S.ReuseAddr 1
+    S.bind sock (S.addrAddress addr)
+    S.listen sock 10
+    putStrLn . show =<< S.getSocketName sock
+    return sock
 
-    streamsOf client = do
-        (is, os) <- (Streams.socketToStreams client)
-        eis <- BStreams.decodeInputStream is
-        eos <- BStreams.encodeOutputStream os
+accepter :: S.Socket -> IO ()
+accepter listener = do
+    void $ forever $ do
+        (client, x) <- S.accept listener
+        putStrLn $ show (client, x)
+        C.forkIO $ do
+            E.bracket (streamsOf client) (const $ S.close client) handleClient
+
+streamsOf :: (Binary.Binary a, Binary.Binary b) => S.Socket -> IO (Streams.InputStream a, Streams.OutputStream b)
+streamsOf client = do
+    (is, os) <- (Streams.socketToStreams client)
+    eis <- BStreams.decodeInputStream is
+    eos <- BStreams.encodeOutputStream os
 
 
-        return (eis, eos)
+    return (eis, eos)
 
-    handleClient :: (Streams.InputStream Lib.Message, Streams.OutputStream Lib.Message)  -> IO ()
-    handleClient (is,os) = do
-        it <- Streams.read is
-        case it of
-            Just Lib.Bing -> do
-                putStrLn $ show Lib.Bing
-                Streams.write (Just Lib.Bong) os
-                handleClient (is,os)
-            Just Lib.Bong -> do
-                putStrLn $ show Lib.Bong
-                Streams.write (Just Lib.Bing) os
-                handleClient (is,os)
-            Nothing -> Streams.write Nothing os
+handleClient :: (Streams.InputStream Lib.Message, Streams.OutputStream Lib.Message)  -> IO ()
+handleClient (is,os) = do
+    it <- Streams.read is
+    case it of
+        Just Lib.Bing -> do
+            putStrLn $ show Lib.Bing
+            Streams.write (Just Lib.Bong) os
+            handleClient (is,os)
+        Just Lib.Bong -> do
+            putStrLn $ show Lib.Bong
+            Streams.write (Just Lib.Bing) os
+            handleClient (is,os)
+        Nothing -> Streams.write Nothing os
