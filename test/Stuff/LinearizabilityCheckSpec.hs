126f3abb5cd79ec119044648dc0d6f61f11ec54d
Ceri Storey <cez@necrofish.org.uk>
2018-04-09 21:42:10 +0100

    Update test, and note why my code is wrong.
    Currently, seems to check for serializability, wheras as per
    http://www.bailis.org/blog/linearizability-versus-serializability/ ,
    Linarizability retains a wall-clock order where under serializablity,
    operations can be re-ordered.

--- test/Stuff/LinearizabilityCheckSpec.hs
+++ test/Stuff/LinearizabilityCheckSpec.hs
@@ -86,26 +86,26 @@ checkHistory :: forall req res s. (Eq req, Eq res, Show req, Show res, Show s)
              -> s
              -> [(Process, HistoryElement req res)]
              -> Either () [Linearization req res]
-checkHistory model initialState history = case go [] Map.empty Map.empty initialState byProcess of
+checkHistory model initialState history = case go Map.empty Map.empty initialState byProcess of
     h : _ -> Right h
     _ -> Left ()
   where
     byProcess :: Map Process [HistoryElement req res]
     byProcess = foldl' (flip $ \(p, op) -> Map.insertWith (flip mappend) p [op]) Map.empty history
 
-    go :: [Linearization req res]
-       -> Map Process req
+    go :: Map Process req
        -> Map Process res
        -> s
        -> Map Process [HistoryElement req res]
        -> [[Linearization req res]] -- Set of potential options
-    go prev calls rets s histories = doneRule <|> callRule <|> linRule <|> retRule
+    go calls rets s histories = doneRule <|> callRule <|> linRule <|> retRule
 
       where
       candidates = foldMap (\(p, h) -> map (\op -> (p, op)) $ take 1 h) $ Map.toList histories
-      doneRule =
+      doneRule = do
+        -- Trace.traceM (Text.unpack $ Text.format "done? calls:{}; rets: {}; pending:{}" (Text.Shown $ Map.size calls, Text.Shown $ Map.size rets, Text.Shown $ Map.map length histories))
         if candidates == [] && Map.null calls && Map.null rets
-        then return prev
+        then return []
         else empty
 
         -- From Testing from "Testing for Linearizability", Gavin Lowe
@@ -114,8 +114,8 @@ checkHistory model initialState history = case go [] Map.empty Map.empty initial
         (p, op) <- candidates
         case op of
           Call req | Map.notMember p calls && Map.notMember p rets -> do
-            -- Trace.trace (Text.unpack $ Text.format "call@{}: req:{}" (Text.Shown p, Text.Shown req)) $ return ()
-            go prev (Map.insert p req calls) rets s $ Map.adjust (drop 1) p histories
+            -- Trace.traceM (Text.unpack $ Text.format "call@{}: req:{}" (Text.Shown p, Text.Shown req))
+            go (Map.insert p req calls) rets s $ Map.adjust (drop 1) p histories
           _ -> empty
 
       linRule = do
@@ -124,25 +124,36 @@ checkHistory model initialState history = case go [] Map.empty Map.empty initial
         (p, _) <- candidates
         req <- maybe empty pure $ Map.lookup p calls
         let (s', ret) = model s req
-        -- Trace.trace (Text.unpack $ Text.format "lin@{}: state: {}: req:{} -> expected:{}" (Text.Shown p, Text.Shown s, Text.Shown req, Text.Shown ret)) $ return ()
-        go (Op p req ret : prev) (Map.delete p calls) (Map.insert p ret rets) s' histories
+        -- Trace.traceM (Text.unpack $ Text.format "lin@{}: state: {}: req:{} -> expected:{}" (Text.Shown p, Text.Shown s, Text.Shown req, Text.Shown ret))
+        rest <- go (Map.delete p calls) (Map.insert p ret rets) s' histories
+        return $ Op p req ret : rest
 
       retRule = do
         (p, op) <- candidates
         expected <- maybe empty pure $ Map.lookup p rets
         case op of
           Ret res | res == expected -> do
-            -- Trace.trace (Text.unpack $ Text.format "ret@{}: res:{} == expected:{}" (Text.Shown p, Text.Shown res, Text.Shown expected)) $ return ()
-            go prev calls (Map.delete p rets) s $ Map.adjust (drop 1) p histories
+            -- Trace.traceM (Text.unpack $ Text.format "ret@{}: res:{} == expected:{}" (Text.Shown p, Text.Shown res, Text.Shown expected))
+            go calls (Map.delete p rets) s $ Map.adjust (drop 1) p histories
+          Ret _res -> do
+            -- Trace.traceM (Text.unpack $ Text.format "ret@{}: res:{} != expected:{}" (Text.Shown p, Text.Shown _res, Text.Shown expected))
+            empty
           _ -> empty
 
 spec :: Spec
 spec = do
   describe "Examples from Herlihy and Wing" $ do
     it "Linearizes h5" $ do
-      pending
       (checkHistory register newRegister h5History :: Either () [Linearization (RegisterReq Int) (RegisterRet Int)]) `shouldBe` Right h5Linearisation
     it "Finds h6 Invalid" $ do
+      {-
+        If I say `shouldBe` Right h5Linearisation; we see:
+
+        expected: Right [Op (Process 0) (Write 0) Ok,Op (Process 1) (Write 1) Ok,Op (Process 0) Read (Val 1),Op (Process 2) (Write 0) Ok,Op (Process 1) Read (Val 0)]
+        but got:  Right [Op (Process 0) (Write 0) Ok,Op (Process 1) (Write 1) Ok,Op (Process 0) Read (Val 1),Op (Process 1) Read (Val 1),Op (Process 2) (Write 0) Ok]
+
+        So the final Write from `c` and Read from `b` end up getting re-ordered. So, clearly, we have a missing constraint on the concrete ordering.
+      -}
       pending
       checkHistory register newRegister h6History `shouldBe` Left ()
 
